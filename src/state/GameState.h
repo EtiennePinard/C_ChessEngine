76a2d4688e438cc2f97498a84f702dea832b4e2d
EJRP123 <e1j2r3p4@gmail.com>
2024-01-21 21:20:35 -0500

    Added Zobrist hashing and a ChessGame struct to contain the previous hashed states

--- src/state/GameState.h
+++ src/state/GameState.h
@@ -3,29 +3,57 @@
 
 #include <stddef.h>
 #include "Board.h"
+#include "Move.h"
+
+typedef struct {
+    // Rank is already included in the side to move key
+    u64 enPassantFile[9];
+    // 16 total possibilities for castling
+    u64 castlingPerms[16];
+    // 14 is the biggest piece index ((Black pawn piece index - 9) + 1)
+    // To access the random number for a piece for a square: pieces[piece - 9][square]
+    u64 pieces[14][BOARD_SIZE];
+    
+    u64 sideToMove;
+} ZobristRandomNumber;
+
+typedef u64 ZobristKey;
 
 typedef struct {
     Board board;
     PieceCharacteristics colorToGo;
-    int castlingPerm, // The first bit is for white king side, second bit is for white queen side and pattern continues but for black
-        enPassantTargetSquare, 
-        turnsForFiftyRule, 
-        nbMoves;
-} GameState;
+    char castlingPerm; // 4 bits are used. The first bit is for white king side, second bit is for white queen side and pattern continues but for black
+    int enPassantTargetSquare; 
+    int turnsForFiftyRule; 
+    int nbMoves;
+    ZobristKey key;
+} ChessPosition;
 
 /**
- * Returns the number of elements in an array of gamestates.
- * Assumes that the last element of the array is 0
+ * Represents a game of chess.
+ * currentState holds all information of a position (same information that is in a fen string)
+ * Zobrist keys are used to quickly compute repetitions
 */
-size_t nbGameStatesInArray(GameState* gameStates);
+typedef struct {
+    ChessPosition* currentState;
+    
+    ZobristKey* previousStates;
+    int previousStatesCount;
+    int previousStatesCapacity;
+} ChessGame;
 
-GameState* createState(
-    Board board,
-    int colorToGo, 
-    int castlingPerm, 
-    int enPassantTargetSquare, 
-    int turnsForFiftyRule, 
-    int nbMoves
-);
+ZobristRandomNumber* zobristRandomNumber;
+
+ChessGame* newChessGame(ChessGame* result, char* fenString);
+void freeChessGame(ChessGame* chessGame);
+
+void zobristKeyInitialize();
+void zobristKeyTerminate();
+
+/**
+ * This function should be called only once when creating the your initial ChessGame struct
+ * Subsequent Zobrist key will be calculated by the makeMove function incrementally
+*/
+void calculateZobristKey(ChessPosition* state);
 
 #endif /* BDF83061_7504_461E_BCDD_602085692048 */
--- src/state/gameState.c
+++ src/state/gameState.c
@@ -1,31 +1,84 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <assert.h>
 #include "GameState.h"
-#include "stdlib.h"
-#include "assert.h"
+#include "../utils/FenString.h"
 
-size_t nbGameStatesInArray(GameState* gameStates) {
-  size_t size = 0;
-  // I just check that colorToGo == 0
-  // If that is the case, then the GameState is not valid, as WHITE = 8 and BLACK = 16
-  while (gameStates[size].colorToGo == 0) {
-    size++;
-  }
-  return size;
+ChessGame* newChessGame(ChessGame* result, char* fenString) {
+    if (result == NULL) {
+        result = (ChessGame*) malloc(sizeof(ChessGame));
+        assert(result != NULL && "Buy more RAM lol");
+    }
+
+    ChessPosition* currentPosition = (ChessPosition*) malloc(sizeof(ChessPosition));
+    assert(currentPosition != NULL && "Buy more RAM lol");
+    if (!setChessPositionFromFenString(fenString, currentPosition)) {
+        printf("Invalid fen string %s\n", fenString);
+        return NULL;
+    }
+    result->currentState = currentPosition;
+
+    result->previousStatesCapacity = 256;
+    result->previousStatesCount = 0;
+    result->previousStates = malloc(sizeof(ZobristKey) * result->previousStatesCapacity);
+    assert(result->previousStates != NULL && "Buy more RAM lol");
+
+    return result;
 }
 
-GameState* createState(
-    Board board,
-    int colorToGo, 
-    int castlingPerm, 
-    int enPassantTargetSquare, 
-    int turnsForFiftyRule, 
-    int nbMoves) {
-    GameState* result = malloc(sizeof(GameState));
-    assert(result != NULL && "Malloc failed so buy more RAM lol");
-    result->board = board;
-    result->castlingPerm = castlingPerm;
-    result->colorToGo = colorToGo;
-    result->enPassantTargetSquare = enPassantTargetSquare;
-    result->nbMoves = nbMoves;
-    result->turnsForFiftyRule = turnsForFiftyRule;
-    return result;
+void freeChessGame(ChessGame* chessGame) {
+    free(chessGame->currentState);
+    free(chessGame->previousStates);
+    free(chessGame);
+}
+
+void zobristKeyInitialize() {
+    srand(time(NULL));
+    zobristRandomNumber = malloc(sizeof(ZobristRandomNumber));
+    assert(zobristRandomNumber != NULL && "Buy more RAM lol");
+
+    for (int color = 0; color < 2; color++) {
+        PieceCharacteristics pieceColor = color == 0 ? WHITE : BLACK;
+        for (PieceCharacteristics piece = KING; piece <= PAWN; piece++) {
+            Piece index = makePiece(pieceColor, piece) - 9;
+            for (int square = 0; square < BOARD_SIZE; square++) {
+                zobristRandomNumber->pieces[(int) index][square] = random_u64();
+            }
+        }
+    }
+    for (int i = 0; i < 16; i++) {
+        zobristRandomNumber->castlingPerms[i] = random_u64();
+    }
+    for (int i = 0; i < 9; i++) {
+        zobristRandomNumber->enPassantFile[i] = i == 0 ? (u64) 0 : random_u64();
+    }
+    zobristRandomNumber->sideToMove = random_u64();
+}
+
+void zobristKeyTerminate() {
+    free(zobristRandomNumber);
+}
+
+// Only use this function to set the initial Zobrist key
+// The subsequent keys are computed incrementally by the makeMove function
+void calculateZobristKey(ChessPosition* state) {
+    u64 zobristKey = (u64) 0;
+
+    for (int square = 0; square < BOARD_SIZE; square++) {
+        Piece piece = pieceAtIndex(state->board, square);
+        if (piece != NOPIECE) {
+            zobristKey ^= zobristRandomNumber->pieces[(int) piece - 9][square];
+        }
+    }
+
+    zobristKey ^= zobristRandomNumber->enPassantFile[state->enPassantTargetSquare];
+
+    if (state->colorToGo == WHITE) {
+        zobristKey ^= zobristRandomNumber->sideToMove;
+    }
+
+    zobristKey ^= zobristRandomNumber->castlingPerms[(int) state->castlingPerm];
+
+    state->key = zobristKey;
 }

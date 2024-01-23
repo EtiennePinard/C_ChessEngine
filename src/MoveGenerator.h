4fa9120c7730d34d842471ed76f5a7b40797ff56
EJRP123 <e1j2r3p4@gmail.com>
2024-01-22 22:09:00 -0500

    Made information on the current position available publicly

--- src/MoveGenerator.h
+++ src/MoveGenerator.h
@@ -6,6 +6,22 @@
 #include "state/Move.h"
 
 /**
+ * Tells which squares are attacked by ennemy pieces for a turn
+ * Only valid after getValidMoves is called
+*/
+extern bool attackedSquares[BOARD_SIZE];
+/**
+ * If the friendly king (for the current turn) is in check
+ * Only valid after getValidMoves is called
+*/
+extern bool inCheck;
+/**
+ * If the friendly king (for the current turn) is in double check
+ * Only valid after getValidMoves is called
+*/
+extern bool inDoubleCheck;
+
+/**
  * Computes the valid moves in a given position and stores the moves in the result array
  * and the amount of moves in the numMoves pointer
 */
